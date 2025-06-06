241f6d285a6bc4bf60776703c3b030f06f3926bc
mh <106450840+usernamesaresosilly@users.noreply.github.com>
2025-06-05 22:30:08 -0700

    Update cam_generation.h

--- project/cam_generation.h
+++ project/cam_generation.h
@@ -1,8 +1,10 @@
-#ifndef MESSAGE_GENERATOR_H
-#define MESSAGE_GENERATOR_H
+#ifndef CAM_GENERATION_H
+#define CAM_GENERATION_H
 
 #include <string>
 
+// Generates n CAM-style messages as a single string.
+// Returns an error message if n is out of bounds.
 std::string generate_messages(int n);
 
-#endif // MESSAGE_GENERATOR_H
+#endif // CAM_GENERATION_H
